452df3d212dca5e32f9772b4576e88aff0606861
lauren wright <wright.lauren20@gmail.com>
2018-09-23 12:58:47 -0400

    confirmed/unconfirmed

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ class App extends Component {
     guests: [
       {
         name: 'Treasure',
-        isConfirmed: false,
+        isConfirmed: false
       },
       {
         name: 'Nic',
-        isConfirmed: true,
+        isConfirmed: true
       },
       {
         name: 'Matt K',
-        isConfirmed: false,
+        isConfirmed: false
       }
     ]
-  }
+  };
 
   toggleConfirmationAt = indexToChange =>
     this.setState({
@@ -35,51 +35,50 @@ class App extends Component {
     });
 
   getTotalInvited = () => this.state.guests.length;
-  // get AttendingGuests = () =>
+  // getAttendingGuests = () =>
   // getUnconfirmedGuests = () =>
 
   render() {
     return (
-    <div className="App">
-      <header>
-        <h1>RSVP</h1>
-        <p>A Treehouse App</p>
-        <form>
+      <div className="App">
+        <header>
+          <h1>RSVP</h1>
+          <p>A Treehouse App</p>
+          <form>
             <input type="text" value="Safia" placeholder="Invite Someone" />
             <button type="submit" name="submit" value="submit">Submit</button>
-        </form>
-      </header>
-      <div className="main">
-        <div>
-          <h2>Invitees</h2>
-          <label>
-            <input type="checkbox" /> Hide those who haven't responded
-          </label>
+          </form>
+        </header>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox" /> Hide those who haven't responded
+            </label>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <GuestList
+            guests={this.state.guests}
+            toggleConfirmationAt={this.toggleConfirmationAt} />
+
         </div>
-        <table className="counter">
-          <tbody>
-            <tr>
-              <td>Attending:</td>
-              <td>2</td>
-            </tr>
-            <tr>
-              <td>Unconfirmed:</td>
-              <td>1</td>
-            </tr>
-            <tr>
-              <td>Total:</td>
-              <td>3</td>
-            </tr>
-          </tbody>
-        </table>
-
-        <GuestList
-          guests={this.state.guests} 
-          toggleConfirmationAt={this.toggleConfirmationAt}
-        />
-
       </div>
-    </div>
     );
   }
 }
